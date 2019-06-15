813fa0b214eb0efe898d05c7a605b1a6af14ad02
Xiaojiuc <742139017@qq.com>
2019-06-15 23:00:00 +0800

    test

---
+++ src/main/java/com/qhit/ThreadTest/TestSS.java
@@ -0,0 +1,12 @@
+package com.qhit.ThreadTest;
+
+/**
+ * @Date:2019/6/15
+ * @Description:
+ * @version:1.0
+ */
+public class TestSS {
+        public static void main(String[] args){
+            //方法体
+        }
+}
