4b61211ab0c790a41b17c1b0f510258d7c367334
Xiaojiuc <742139017@qq.com>
2019-06-15 22:56:30 +0800

    test

--- src/main/java/com/qhit/ThreadTest/ThreadTest.java
+++ src/main/java/com/qhit/ThreadTest/ThreadTest.java
@@ -12,6 +12,6 @@ public class ThreadTest implements Runnable {
     }
     @Override
     public void run() {
-        System.out.println("sh:"+a);
+        System.out.println("sh:"+a);asd
     }
 }
